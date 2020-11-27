b4bd460c251047b9c3b80eebf492afd9ea0f85a7
Sambit Sanyal <51664438+sambyte99@users.noreply.github.com>
2020-11-27 06:07:54 +0530

    Add files via upload
    I am single. So is my commit singular

---
+++ server/app.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('./Employee')
+
+app.use(bodyParser.json())
+
+const Employee = mongoose.model("employee")
+
+//PiIbP3VFHHPPDxEa 
+//1630gNVkzHS1IEMs
+const mongoUri = "mongodb+srv://sam:1630gNVkzHS1IEMs@cluster0.eucrs.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("Connected to mongo db successfully")
+})
+mongoose.connection.on("error",(err)=>{
+    console.log("error",err)
+})
+
+app.get('/',(req,res)=>{
+    Employee.find({}).then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        console.log(err)
+    })
+    
+    
+})
+
+
+app.post('/send-data',(req,res)=>{
+     const employee = new Employee({
+         name:req.body.name,
+         email:req.body.email,
+         phone:req.body.phone,
+         picture:req.body.picture,
+         salary:req.body.salary,
+         position:req.body.position
+
+     })
+     employee.save()
+     .then(data=>{
+         console.log(data)
+         res.send(data)
+     }).catch(err=>{
+         console.log(err)
+     })
+     
+})
+
+app.post('/delete',(req,res)=>{
+    Employee.findByIdAndRemove(req.body.id)
+    .then(data=>{
+        console.log(data)
+        res.send(data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+app.post('/update',(req,res)=>{
+    Employee.findByIdAndUpdate(req.body.id,{
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        picture:req.body.picture,
+        salary:req.body.salary,
+        position:req.body.position
+    }).then(data=>{
+        console.log(data)
+        res.send(data)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("server running")
+})
